f6c3291976c11a3cd8fb7a49793c4f83f6a0175e
SoinSoin <vincent.retore@gmail.com>
2018-03-15 17:17:19 +0100

    first and end

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function (){
+
+    $("#réserverG").on("click", function (){
+    
+        $(".modal-title").html("Hotel de la gare");
+
+    });
+
+    $("#réserverP").on("click", function (){
+    
+        $(".modal-title").html("Hotel du port");
+
+    });
+
+    $("#réserverA").on("click", function (){
+    
+        $(".modal-title").html("Hotel de l'angle de la rue");
+
+    });
+
+
+});
